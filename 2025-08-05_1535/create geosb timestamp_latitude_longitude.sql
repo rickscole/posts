743b544dcb918aff1584893375b12636e04da8eb
rickscole <ricksaubercole@gmail.com>
2025-08-05 17:03:35 -0400

    Add id column

--- 2025-08-05_1535/create geosb timestamp_latitude_longitude.sql
+++ 2025-08-05_1535/create geosb timestamp_latitude_longitude.sql
@@ -2,7 +2,8 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [geosb].[timestamp_latitude_longitude](
+CREATE TABLE [geosb].[synthetic_movement_dataset_anomalous](
+	[id] [smallint] NULL,
 	[timestamp] [datetime2](7) NULL,
 	[latitude] [float] NULL,
 	[longitude] [float] NULL
