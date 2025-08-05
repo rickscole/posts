541020e7adaebd7fac99faafe344774cc26c4979
rickscole <ricksaubercole@gmail.com>
2025-08-05 17:05:40 -0400

    Update create geosb timestamp_latitude_longitude.sql
    table name was wrong, so I changed it

--- 2025-08-05_1535/create geosb timestamp_latitude_longitude.sql
+++ 2025-08-05_1535/create geosb timestamp_latitude_longitude.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [geosb].[synthetic_movement_dataset_anomalous](
+CREATE TABLE [geosb].[timestamp_latitude_longitude](
 	[id] [smallint] NULL,
 	[timestamp] [datetime2](7) NULL,
 	[latitude] [float] NULL,
