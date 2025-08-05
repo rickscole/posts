d8378516279d97226269a1a8c5b301deaeaad205
rickscole <ricksaubercole@gmail.com>
2025-08-05 16:17:06 -0400

    Create create geosb timestamp_latitude_longitude.sql

---
+++ 2025-08-05_1535/create geosb timestamp_latitude_longitude.sql
@@ -0,0 +1,10 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [geosb].[timestamp_latitude_longitude](
+	[timestamp] [datetime2](7) NULL,
+	[latitude] [float] NULL,
+	[longitude] [float] NULL
+) ON [PRIMARY]
+GO
