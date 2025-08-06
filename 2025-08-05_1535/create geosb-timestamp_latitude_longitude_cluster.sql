3726c2b04993899dc1fb50f49dd96fbdc9921007
rickscole <ricksaubercole@gmail.com>
2025-08-06 09:45:36 -0400

    Create create geosb-timestamp_latitude_longitude_cluster.sql

---
+++ 2025-08-05_1535/create geosb-timestamp_latitude_longitude_cluster.sql
@@ -0,0 +1,9 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [geosb].[timestamp_latitude_longitude_cluster](
+	[id] [smallint] NULL,
+	[cluster_id] [smallint] NULL
+) ON [PRIMARY]
+GO
