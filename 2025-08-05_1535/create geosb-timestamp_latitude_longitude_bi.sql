f9e13024c4795b8b2c10358245f77425e8f774e8
rickscole <ricksaubercole@gmail.com>
2025-08-06 09:52:48 -0400

    Update create geosb-timestamp_latitude_longitude_bi.sql

--- 2025-08-05_1535/create geosb-timestamp_latitude_longitude_bi.sql
+++ 2025-08-05_1535/create geosb-timestamp_latitude_longitude_bi.sql
@@ -1,9 +1,11 @@
 create view geosb.timestamp_latitude_longitude_bi as 
 select 
 a.*
-, year = year(timestamp)
-, month = month(timestamp)
-, day = day(timestamp)
-, hour = datepart(hour, timestamp)
+, year = year(a.timestamp)
+, month = month(a.timestamp)
+, day = day(a.timestamp)
+, hour = datepart(hour, a.timestamp)
 , lat_long = concat(cast(a.latitude as nvarchar(50)), ',',cast(a.longitude as nvarchar(50)))
+, b.cluster_id
 from [geosb].[timestamp_latitude_longitude] a 
+left join [geosb].[timestamp_latitude_longitude_cluster] b on a.id = b.id
